bc7940afadf34850061a22ec1a01024c57075434
centralrosinhas-gif <centralrosinhas@gmail.com>
2025-12-16 10:43:07 -0300

    Add files via upload

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
 const prismaConfig = {
-  adapter: process.env.DATABASE_URL,
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL
+    }
+  }
 }
 
 export const prisma = new PrismaClient(prismaConfig)
